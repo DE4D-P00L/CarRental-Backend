8e23b80c03fb8dad34a62f79510c4e0f81a4c871
Prashant <jatinverma200025@gmail.com>
2024-02-25 17:07:46 +0530

    Fixed Cors error

--- server.js
+++ server.js
@@ -12,13 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 //middlewares
 // TODO: add cors config
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
+app.use(cors({}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
